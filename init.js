1e3fc0414048325fe8afa61f6b83653be9598728
Giga Bokuchava <mister_giga@live.com>
2023-04-29 13:18:56 +0400

    Update init.js

--- init.js
+++ init.js
@@ -30,8 +30,6 @@ window.getPlaylistFromId = async function(id) {
     let isInString = false;
     let i = startIndex + playlistVideoListRendererKeyName.length + 1;
     
-    console.log(playlistVideoListRendererKeyName, html.length, startIndex, i);
-    
     for (; i < html.length && level > 0; i++)
     {
         const ch = html[i];
@@ -58,5 +56,41 @@ window.getPlaylistFromId = async function(id) {
     return playlist;
     
 }
-const f = (async () => console.log( await window.getPlaylistFromId("PLLGmt3bXA_93pvHgKm7dbEvW410pDFKKl")));
+
+window.getPlaylistMoreVideos = async function(continuationToken){
+    const playListLoadMoreUrl = "https://www.youtube.com/youtubei/v1/browse?prettyPrint=false";
+            
+
+    const requestObj = {
+        context: {
+            client: {
+                clientName: "WEB",
+                clientVersion: "2.20230414.01.00"
+            }
+        },
+        continuation: continuationToken
+    };
+
+    const resp = await fetch(playListLoadMoreUrl, {
+        method: 'POST',
+        headers: {
+            'User-Agent': 'PostmanRuntime/7.32.2'
+        },
+        body: JSON.stringify(requestObj)
+    });
+    let jsonObj = await resp.json();
+    jsonObj = jsonObj.onResponseReceivedActions[0].appendContinuationItemsAction;
+    return {
+        id: jsonObj.targetId,
+        videos: window.getVideos(jsonObj, 'continuationItems'),
+        continuationToken: window.getContinuationToken(jsonObj,'continuationItems')
+    };
+    
+}
+
+const f = (async () => {
+    const initialPlayList = await window.getPlaylistFromId("PLLGmt3bXA_93pvHgKm7dbEvW410pDFKKl");
+    const more = await window.getPlaylistMoreVideos(initialPlayList.continuationToken);
+    console.log(more);
+});
 f();
